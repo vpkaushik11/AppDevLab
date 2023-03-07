0986552f56b875e861bb4e20b2acfd2368a66414
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2023-03-07 14:46:45 +0530

    Lab 6: Added base code for file upload

--- Lab 6/app.js
+++ Lab 6/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const userModel = require("./userData");
+const multer = require("multer");
+const uuid = require("uuid").v4;
+const fs = require("fs");
 
 const app = express();
 const dbURI = "mongodb://localhost:27017/AppDevLab";
@@ -96,10 +99,52 @@ app.post('/login', (req, res) => {
         });
 });
 
+// File Upload Service Part
+
+const dir = "./uploads";
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, {
+        recursive: true,
+    });
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // to select a destination
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        // to get the file name
+        const { originalname } = file;
+        cb(null, `${uuid()}-${originalname}`);
+    },
+});
+
+const upload = multer({ storage: storage });
+
 app.get('/home', (req, res) => {
     res.render("upload", { client });
 });
 
-app.post('/upload', (req, res) =>{
-    
+app.post("/upload", upload.single("avatar"), (req, res) => {
+    console.log(storage.getFilename().originalname);
+    return res.json({ status: "OK"});
+});
+
+//joining path of directory
+const directoryPath = path.join(__dirname, "uploads");
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+  //handling error
+  if (err) {
+    return console.log("Unable to scan directory: " + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+    // Do whatever you want to do with the file
+    console.log(file);
+    app.get(`/file-${file}`, (req, res) => {
+      res.sendFile(path.join(__dirname, "uploads", file));
+    });
+  });
 });
